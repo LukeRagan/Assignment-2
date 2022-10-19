2c2ebeac74c9df05cbff1dbd369b9a0c31b31f85
Luke Ragan <114009922+LukeRagan@users.noreply.github.com>
2022-10-18 22:29:10 -0500

    Add files via upload

---
+++ CS303Project.cpp
@@ -0,0 +1,107 @@
+#include "CS303_1.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm> 
+
+
+
+int main()
+{
+    const int ARRAY_SIZE = 100;
+    int num[ARRAY_SIZE];
+    int count = 0;
+    int toFind;
+    int newValue;
+    int i;
+    const int ARRAY_SIZE2 = 101;
+    int newArray[ARRAY_SIZE2];
+
+    // prompts the user to enter the integer they want to find in the array.
+    std::cout << "Please enter the integer you want to find: ";
+    std::cin >> toFind;
+
+    ifstream inputFile;
+
+    // opens the text file.    
+    inputFile.open("Text.txt");
+
+    // Read the numbers from the file into the array.
+    while (count < ARRAY_SIZE && inputFile >> num[count]) {
+        count++;
+    }
+
+    // Close the file.
+    inputFile.close();
+
+    // Finds the whatever number the user inputed into the array and then using std::distance finds the location of the number in the array.
+    int* found = std::find(std::begin(num), std::end(num), toFind);
+    if (found != std::end(num))
+    {
+        int spaceInArray = std::distance(num, found);
+        std::cout << "The integer " << toFind << " is in index location " << spaceInArray << "\n";
+    }
+    else
+    {
+        std::cout << "The integer is not in the array.\n";
+    }
+    // Asks the user to input whatever location in the array they want to delete.
+    std::cout << "What index do you want to delete: ";
+    int valueZero;
+    std::cin >> valueZero;
+    // implementation of the void function in the .h file to delete the index that the user chose.
+    Delete(num, ARRAY_SIZE, valueZero);
+    if (valueZero > ARRAY_SIZE) {
+        std::cout << " ";
+    }
+    else {
+        for (i = 0; i < 99; i++) {
+            std::cout << num[i] << " ";
+        }
+    }
+    std::cout << " " << "\n";
+
+    try {
+        std::cout << "What index in the array do you want to change: ";
+        int intChanged;
+        std::cin >> intChanged;
+        std::cout << "What do you want it changed to: ";
+        int ChangedTo;
+        std::cin >> ChangedTo;
+        if (ChangedTo > 0) {
+            for (int i = 0; i < ARRAY_SIZE; i++) {
+                num[intChanged] = ChangedTo;
+                std::cout << num[i] << " ";
+            }
+            std::cout << " " << "\n";
+
+            std::cout << "The old index " << intChanged << " is now " << num[intChanged] << "\n";
+        }
+        else {
+            throw(ChangedTo);
+        }
+    }
+    catch (int changedNum) {
+        cout << "Cannot use that number.\n";
+    }
+
+    try {
+        std::cout << "What integer do you want to add to the end of the array: ";
+        int newInt;
+        std::cin >> newInt;
+        if (newInt > 0) {
+            std::copy(num, num + ARRAY_SIZE2, newArray);
+
+            for (int i = 0; i < ARRAY_SIZE2; i++) {
+                newArray[100] = newInt;
+                std::cout << newArray[i] << " ";
+            }
+        }
+        else {
+            throw (newInt);
+        }
+    }
+    catch (int myNum) {
+        cout << "Cannot use that number.\n";
+    }
+    return 0;
+}
